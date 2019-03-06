901297f61dc0f9df34a34ed43fc61db1b9fefa64
Aladdin Persson <Aladdin.Persson@hotmail.com>
2019-03-06 21:48:17 +0100

    test for sorting

--- Algorithm_tests/sorting_tests/test_sorting.py
+++ Algorithm_tests/sorting_tests/test_sorting.py
@@ -1,30 +1,41 @@
-# import alla sorting algoritms
+# Import folder where sorting algorithms
+import sys
+sys.path.append('../../Algorithms/sorting')
+
+# from bubblesort import bubblesort
+
+from bubblesort import bubblesort
 
 L1 = [1,2,3,4,5,6,7,8,9]
-L1_sorted = []
+L1_sorted = sorted(L1)
+
 L2 = [9,8,7,6,5,4,3,2,1]
-L2_sorted = []
+L2_sorted = sorted(L2)
+
 L3 = [1,1,1,1,1,1,1,1,1]
-L3_sorted = []
+L3_sorted = sorted(L3)
+
 L4 = [6,7,3,5,1,3]
-L4_sorted = [1,3,3,5,6,7]
+L4_sorted = sorted(L4)
+
 L5 = []
 L5_sorted = []
 
 
 # Bubblesort
-assert sorted(L4) == L4_sorted, "Not correctly sorted"
-assert sorted(L4) == L4_sorted, "Not correctly sorted"
-assert sorted(L4) == L4_sorted, "Not correctly sorted"
-assert sorted(L4) == L4_sorted, "Not correctly sorted"
-assert sorted(L4) == L4_sorted, "Not correctly sorted"
+def test_bubblesort():
+    assert bubblesort(L1) == L1_sorted, "Not correctly sorted"
+    assert bubblesort(L4) == L4_sorted, "Not correctly sorted"
+    assert bubblesort(L4) == L4_sorted, "Not correctly sorted"
+    assert bubblesort(L4) == L4_sorted, "Not correctly sorted"
+    assert bubblesort(L4) == L4_sorted, "Not correctly sorted"
 
 # Selectionsort
-assert sorted(L4) == L4_sorted, "Not correctly sorted"
-assert sorted(L4) == L4_sorted, "Not correctly sorted"
-assert sorted(L4) == L4_sorted, "Not correctly sorted"
-assert sorted(L4) == L4_sorted, "Not correctly sorted"
-assert sorted(L4) == L4_sorted, "Not correctly sorted"
+# assert sorted(L4) == L4_sorted, "Not correctly sorted"
+# assert sorted(L4) == L4_sorted, "Not correctly sorted"
+# assert sorted(L4) == L4_sorted, "Not correctly sorted"
+# assert sorted(L4) == L4_sorted, "Not correctly sorted"
+# assert sorted(L4) == L4_sorted, "Not correctly sorted"
 
 # Heapsort
 
