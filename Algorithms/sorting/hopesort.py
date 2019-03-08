42f63ccfd5688e02fcac1b99b0918a48c2b9b54d
Aladdin Persson <Aladdin.Persson@hotmail.com>
2019-03-08 17:54:53 +0100

    Created my own sorting algorithm =)

---
+++ Algorithms/sorting/hopesort.py
@@ -0,0 +1,18 @@
+'''
+Hopesort algorithm. If it works, then it works in constant time.
+Use at own risk :)
+
+Programmed by Aladdin Persson <aladdin.persson at hotmail dot com>
+*  2019-03-08 Initial code
+
+'''
+
+def hopesort(L):
+    # hope it is sorted
+    return L
+
+if __name__ == '__main__':
+    unsorted = [1,2,3,4,5,6]
+    # constant time
+    hopefully_sorted = hopesort(unsorted)
+    print(hopefully_sorted)
