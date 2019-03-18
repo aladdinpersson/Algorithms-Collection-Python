4ca5de088b8b1a1e093273b657ed4445ba319282
Aladdin Persson <Aladdin.Persson@hotmail.com>
2019-03-18 20:47:47 +0100

    added prime factorization

---
+++ Algorithms/math/prime_factorization/primefactorization.py
@@ -0,0 +1,32 @@
+'''
+Purpose is given a number write it only as a factorization of primes.
+
+Time complexity: O(sqrt(n))
+
+Programmed by Aladdin Persson <aladdin.persson at hotmail dot com>
+*   2019-03-18 Initial programming
+
+'''
+
+def primefactorization(n):
+    original_value = n
+    values = []
+
+    for i in range(2, int(n**0.5)):
+        # Will not pass this if statement if i is not a prime number.
+        # (This is because all numbers have a prime factorization)
+        if n % i == 0:
+            count = 0
+
+            while n % i == 0:
+                n /= i
+                values.append(i)
+                count += 1
+
+    if n == 1:
+        print(f'Prime factorization of {original_value} is: {values}')
+    else:
+        print(f'There is no prime factorization because the number {original_value} is a prime')
+
+if __name__ == '__main__':
+    primefactorization(17)
