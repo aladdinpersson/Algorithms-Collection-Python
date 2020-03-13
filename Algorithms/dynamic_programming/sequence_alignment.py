5568afa99ee072a563915902932307508dc7c3f6
AladdinPerzon <Aladdin.Persson@hotmail.com>
2020-03-13 17:16:32 +0100

    Fixed error in sequence alignment

--- Algorithms/dynamic_programming/sequence_alignment.py
+++ Algorithms/dynamic_programming/sequence_alignment.py
@@ -8,6 +8,9 @@
 # Programmed by Aladdin Persson <aladdin dot persson at hotmail dot com>
 #   2020-02-15 Initial coding
 #   2020-02-16 Improved find_solution and made code cleaner
+#   2020-03-13 There was an error in the code in function find_solution,
+#              I was working with list indexing as if it was a matrix.
+#              Should be working now. Extensive testing would be good.
 
 delta = lambda x,y,i,j: 1 if x[i] != y[j] else 0
 
@@ -23,22 +26,22 @@ def find_solution(OPT, m, n):
 
     if best_choice == insert:
         solution.append('insert_'+str(y[n-1]))
-        return find_solution(OPT[:m+1][:n], m, n-1)
+        return find_solution(OPT, m, n-1)
 
     elif best_choice == align:
         solution.append('align_' + str(y[n-1]))
-        return find_solution(OPT[:m][:n], m-1, n-1)
+        return find_solution(OPT, m-1, n-1)
 
     elif best_choice == delete:
         solution.append('remove_'+str(x[m-1]))
-        return find_solution(OPT[:m][:n+1], m-1, n)
+        return find_solution(OPT, m-1, n)
 
 # alignment takes input strings x,y but also empty list solution to track the specific edits
 def alignment(x, y, solution):
     n = len(y)
     m = len(x)
-    OPT = [ [0 for i in range(n+1)] for j in range(m+1)]
-
+    OPT = [ [j for i in range(n+1)] for j in range(m+1)]
+    
     for i in range(1,m+1):
         OPT[i][0] = i
 
@@ -49,14 +52,14 @@ def alignment(x, y, solution):
         for j in range(1,n+1):
             OPT[i][j] = min(OPT[i-1][j-1] + delta(x,y,i-1,j-1), OPT[i-1][j] + 1, OPT[i][j-1] + 1) #align, delete, insert respectively
 
-    find_solution(OPT,m,n)
+    find_solution(OPT, m, n)
     
     return (OPT[m][n], solution[::-1])
 
 if __name__ == '__main__':
     solution = []
-    x = 'TGACGTGC'
-    y = 'TCGACGTCA'
+    x = 'TGACGTGC'*1
+    y = 'TCGACGTCA'*1
     print('We we want to transform: ' + x + ' to: ' + y)
     min_edit, steps = alignment(x, y, solution)
     print('Minimum amount of edit steps are: ' + str(min_edit))
