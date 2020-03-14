2d07de178bd8900603c01781d9eca603346478f7
Aladdin Persson <Aladdin.Persson@hotmail.com>
2020-03-14 12:40:39 +0100

    Added longest increasing subsequence algorithm (not requirement to be contiguous), will become better at doing tests before adding it to algorithm list. After exam I will try to put some focus on doing extensive testing on the algorithms I've currently implemented as well

---
+++ Algorithms/dynamic_programming/longest_increasing_subsequence.py
@@ -0,0 +1,30 @@
+'''
+O(n^2) algorithm, can be faster and done in O(nlogn), but this works ok.
+To do: Create extensive test cases before adding to algorithm list.
+
+'''
+
+
+def longest_increasing_subsequence(nums):
+    if len(nums) == 0:
+        return 0
+    
+    OPT = [1 for i in range(len(nums))]
+    
+    for i in range(1, len(nums)):
+        for j in range(0, i):
+            if nums[j] < nums[i] and OPT[j] + 1 > OPT[i]:
+                OPT[i] = OPT[j] + 1
+    
+    return max(OPT)
+
+
+if __name__ == '__main__':
+    # test1 = [1,5,-2,10, 50, -10, 10, 1,2,3,4]
+    test2 = [10, 1, 2, 11, 3, 5]
+    # test3 = [10,9,8,5,3,2,1,2,3]
+    # test4 = [1,5,2,3,4,5,6]
+    test5 = []
+    
+    print(test2)
+    print(longest_increasing_subsequence(test2))
