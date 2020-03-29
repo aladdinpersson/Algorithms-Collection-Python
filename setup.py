d7954f869cf3350fbfcf5f38964c862c60735136
Aladdin Persson <Aladdin.Persson@hotmail.com>
2020-03-29 16:00:44 +0200

    try with setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Algorithms',
+    packages=['Algorithm_test'],
+    version='0.0.7',
+    description='Test project to get acquainted with TravisCI',
+    url='https://github.com/AladdinPerzon/Algorithms-Collection-Python',
+)
