810e6d80d689b1905eceec58a1e1a3ca3dcc382e
Aladdin Persson <Aladdin.Persson@hotmail.com>
2020-03-29 16:08:47 +0200

    try with setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='Algorithms',
     packages=['Algorithm_test'],
     version='0.0.7',
-    description='Test project to get acquainted with TravisCI',
+    description='Testing Algorithm Collection',
     url='https://github.com/AladdinPerzon/Algorithms-Collection-Python',
 )
