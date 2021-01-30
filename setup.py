56c38d0fafa6b639d12aa6fe8359292f96162d1d
Aladdin Persson <aladdin.persson@hotmail.com>
2021-01-30 21:53:34 +0100

    changed setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     packages=["Algorithm_test"],
     version="0.0.7",
     description="Testing Algorithm Collection",
-    url="https://github.com/AladdinPerzon/Algorithms-Collection-Python",
+    url="https://github.com/aladdinpersson/Algorithms-Collection-Python",
 )
